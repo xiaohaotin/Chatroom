078184587db43bebcb6a5866134254f98103f921
xiaohaotin <>
2019-07-12 20:17:12 +0900

    chatroom

---
+++ Assets/Bearded Man Studios Inc/Generated/UserGenerated/ChatterManagerBehavior.cs
@@ -0,0 +1,111 @@
+using BeardedManStudios.Forge.Networking;
+using BeardedManStudios.Forge.Networking.Unity;
+using UnityEngine;
+
+namespace BeardedManStudios.Forge.Networking.Generated
+{
+	[GeneratedRPC("{\"types\":[[\"string\", \"string\"]]")]
+	[GeneratedRPCVariableNames("{\"types\":[[\"username\", \"message\"]]")]
+	public abstract partial class chattermanagerBehavior : NetworkBehavior
+	{
+		public const byte RPC_TRANSMITMESSAGE = 0 + 5;
+		
+		public chattermanagerNetworkObject networkObject = null;
+
+		public override void Initialize(NetworkObject obj)
+		{
+			// We have already initialized this object
+			if (networkObject != null && networkObject.AttachedBehavior != null)
+				return;
+			
+			networkObject = (chattermanagerNetworkObject)obj;
+			networkObject.AttachedBehavior = this;
+
+			base.SetupHelperRpcs(networkObject);
+			networkObject.RegisterRpc("transmitmessage", transmitmessage, typeof(string), typeof(string));
+
+			networkObject.onDestroy += DestroyGameObject;
+
+			if (!obj.IsOwner)
+			{
+				if (!skipAttachIds.ContainsKey(obj.NetworkId)){
+					uint newId = obj.NetworkId + 1;
+					ProcessOthers(gameObject.transform, ref newId);
+				}
+				else
+					skipAttachIds.Remove(obj.NetworkId);
+			}
+
+			if (obj.Metadata != null)
+			{
+				byte transformFlags = obj.Metadata[0];
+
+				if (transformFlags != 0)
+				{
+					BMSByte metadataTransform = new BMSByte();
+					metadataTransform.Clone(obj.Metadata);
+					metadataTransform.MoveStartIndex(1);
+
+					if ((transformFlags & 0x01) != 0 && (transformFlags & 0x02) != 0)
+					{
+						MainThreadManager.Run(() =>
+						{
+							transform.position = ObjectMapper.Instance.Map<Vector3>(metadataTransform);
+							transform.rotation = ObjectMapper.Instance.Map<Quaternion>(metadataTransform);
+						});
+					}
+					else if ((transformFlags & 0x01) != 0)
+					{
+						MainThreadManager.Run(() => { transform.position = ObjectMapper.Instance.Map<Vector3>(metadataTransform); });
+					}
+					else if ((transformFlags & 0x02) != 0)
+					{
+						MainThreadManager.Run(() => { transform.rotation = ObjectMapper.Instance.Map<Quaternion>(metadataTransform); });
+					}
+				}
+			}
+
+			MainThreadManager.Run(() =>
+			{
+				NetworkStart();
+				networkObject.Networker.FlushCreateActions(networkObject);
+			});
+		}
+
+		protected override void CompleteRegistration()
+		{
+			base.CompleteRegistration();
+			networkObject.ReleaseCreateBuffer();
+		}
+
+		public override void Initialize(NetWorker networker, byte[] metadata = null)
+		{
+			Initialize(new chattermanagerNetworkObject(networker, createCode: TempAttachCode, metadata: metadata));
+		}
+
+		private void DestroyGameObject(NetWorker sender)
+		{
+			MainThreadManager.Run(() => { try { Destroy(gameObject); } catch { } });
+			networkObject.onDestroy -= DestroyGameObject;
+		}
+
+		public override NetworkObject CreateNetworkObject(NetWorker networker, int createCode, byte[] metadata = null)
+		{
+			return new chattermanagerNetworkObject(networker, this, createCode, metadata);
+		}
+
+		protected override void InitializedTransform()
+		{
+			networkObject.SnapInterpolations();
+		}
+
+		/// <summary>
+		/// Arguments:
+		/// string username
+		/// string message
+		/// </summary>
+		public abstract void transmitmessage(RpcArgs args);
+
+		// DO NOT TOUCH, THIS GETS GENERATED PLEASE EXTEND THIS CLASS IF YOU WISH TO HAVE CUSTOM CODE ADDITIONS
+	}
+}
---
+++ Assets/Chat/ChatterManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using BeardedManStudios.Forge.Networking;
+using BeardedManStudios.Forge.Networking.Generated;
+
+
+public class ChatterManager : ChatManagerBehavior
+{
+    public Transform chatContent;
+    public GameObject chatMessage;
+
+    public void WriteMessage(InputField sender)
+    {
+        if (!string.IsNullOrEmpty(sender.text) && sender.text.Trim().Length > 0)
+        {
+            sender.text = sender.text.Replace("\r", string.Empty).Replace("\n", string.Empty);
+            networkObject.SendRpc(RPC_SEND_MESSAGE, Receivers.All, "Brent", sender.text.Trim());
+            sender.text = string.Empty;
+            sender.ActivateInputField();
+        }
+    }
+
+    public override void SendMessage(RpcArgs args)
+    {
+        string username = args.GetNext<string>();
+        string message = args.GetNext<string>();
+
+        if(string.IsNullOrEmpty(username)|| string.IsNullOrEmpty(message))
+        {
+            // The message or username was empty so do not display this message to anyone
+            return;
+        }
+
+        GameObject newMessage = Instantiate(chatMessage,chatContent);
+        Text content = newMessage.GetComponent<Text>();
+
+        content.text = string.Format(content.text, username, message);
+    }
+
+    
+
+    
+
+    
+}
